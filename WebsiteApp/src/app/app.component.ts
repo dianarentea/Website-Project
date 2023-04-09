03dbda371e53f6f748eaeab49807cc98498b6e0b
dianarentea <rentea88@gmail.com>
2023-04-09 16:22:27 +0300

    added button and route for cities page

--- WebsiteApp/src/app/app.component.ts
+++ WebsiteApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'WebsiteApp';
+
+constructor(private router:Router){}
+
+goToPage(pageName:string):void{
+  this.router.navigate([`${pageName}`]);
+
+}
+
 }
--- WebsiteApp/src/app/app.module.ts
+++ WebsiteApp/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CitiesComponent} from '../app/cities/cities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+
+    //ng-zorro
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
